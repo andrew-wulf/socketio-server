2378fe32fa499c301a61c825cc2b0a16faabff9a
andrew wulf <andrewdwulf@gmail.com>
2025-01-29 18:32:11 -0600

    test files

---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+function search(term, type = 'movie') {
+   let base_url = ENV['THEMOVIEDB_BASE_URL']
+   let api_key = ENV['THEMOVIEDB_API_KEY']
+   let access_token = ENV['THEMOVIEDB_ACCESS_TOKEN']
+}
+
+
+search()
