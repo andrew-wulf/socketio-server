7f2a2b0f2c0e60675fb2ff144cdf4a299337ceb3
andrew wulf <andrewdwulf@gmail.com>
2025-02-02 05:10:16 -0600

    removed / from url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Set up Socket.IO server
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://movie-battle.onrender.com/"], // Allow React frontend
+    origin: ["http://localhost:5173", "https://movie-battle.onrender.com"], // Allow React frontend
     methods: ["GET", "POST"],
   }
 });
